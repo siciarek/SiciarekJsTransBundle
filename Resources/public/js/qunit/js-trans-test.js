1df5900211420bbb21ab7067d441e1192441c3c1
siciarek <siciarek@gmail.com>
2013-11-16 23:39:50 +0100

    Refactoring new transchoice implementation based on the original Fabien inplementation

--- Resources/public/js/qunit/js-trans-test.js
+++ Resources/public/js/qunit/js-trans-test.js
@@ -1,12 +1,22 @@
-
-
-function getSpecificDomainPhrases() {
+/**
+ * String.trans basic test data provider
+ *
+ * @returns {Array}
+ */
+function getTransBasic() {
     return [
-        [ 'Translated: Not Found', 'Not Found', {} ]
+        [ 'Dummy', 'Dummy', 'not existing phrase' ],
+        [ 'Translated: Not Found', 'Not Found', 'phrase' ],
+        [ 'Not Found', 'token.3', 'token' ]
     ];
 }
 
-function getDefaultDomainPhrases() {
+/**
+ * String.trans default domain data provider
+ *
+ * @returns {Array}
+ */
+function getDefaultDomain() {
     return [
         [ 'Dummy', 'Dummy', {}, 'not existing phrase' ],
         [ 'Translated: Not Found', 'Not Found', {}, 'phrase' ],
@@ -18,6 +28,21 @@ function getDefaultDomainPhrases() {
         [ 'Not Found', 'token.3', {}, 'token' ],
         [ 'Hello, John Doe!', 'token.2', {'%username%': 'John Doe'}, 'token with replacement' ],
         [ 'Hello, John Doe! Are you John Doe?', 'token.1', {'%username%': 'John Doe'}, 'token with multiple replacements' ]
+    ];
+}
+
+/**
+ * String.trans given domain data provider
+ *
+ * @returns {Array}
+ */
+function getGivenDomain() {
+    return [
+        [ 'Dummy', 'Dummy', {}, 'TestDomainFirst', 'not existing phrase' ],
+        [ 'Translated: Not Found', 'Not Found', {}, 'TestDomainFirst', 'phrase' ],
+        [ 'Translated: Hello, John Doe!', "Hello, %username%!", {'%username%': 'John Doe'}, 'TestDomainFirst', 'phrase with replacement' ],
+        [ 'Translated: Hello, John Doe! Are you John Doe?', 'Hello, %username%! Are you %username%?', {'%username%': 'John Doe'}, 'TestDomainFirst', 'phrase with mulitiple replacements' ],
+        [ 'Hello, John Doe! You are not John Doe!?', 'Hello, %username%! You are not %username%!?', {'%username%': 'John Doe'}, 'TestDomainFirst', 'replacements only' ]
     ];
 }
 
@@ -37,7 +62,10 @@ function getNonMatchingMessages() {
  * from /vendor/symfony/symfony/src/Symfony/Component/Translation/Tests/MessageSelectorTest.php
  */
 function getChooseTests() {
+
     return [
+        ['There are no apples', 'token.4', 0],
+
         ['There are no apples', '{0} There are no apples|{1} There is one apple|]1,Inf] There are %count% apples', 0],
         ['There are no apples', '{0}     There are no apples|{1} There is one apple|]1,Inf] There are %count% apples', 0],
         ['There are no apples', '{0}There are no apples|{1} There is one apple|]1,Inf] There are %count% apples', 0],
@@ -66,30 +94,97 @@ function getChooseTests() {
         // Indexed only tests which are Gettext PoFile* compatible strings.
         ['There are %count% apples', 'There is one apple|There are %count% apples', 0],
         ['There is one apple', 'There is one apple|There are %count% apples', 1],
-        ['There are %count% apples', 'There is one apple|There are %count% apples', 2]
+        ['There are %count% apples', 'There is one apple|There are %count% apples', 2],
 
         // Tests for float numbers
-//        ['There is almost one apple', '{0} There are no apples|]0,1[ There is almost one apple|{1} There is one apple|[1,Inf] There is more than one apple', 0.7],
-//        ['There is one apple', '{0} There are no apples|]0,1[There are %count% apples|{1} There is one apple|[1,Inf] There is more than one apple', 1],
-//        ['There is more than one apple', '{0} There are no apples|]0,1[There are %count% apples|{1} There is one apple|[1,Inf] There is more than one apple', 1.7],
-//        ['There are no apples', '{0} There are no apples|]0,1[There are %count% apples|{1} There is one apple|[1,Inf] There is more than one apple', 0],
-//        ['There are no apples', '{0} There are no apples|]0,1[There are %count% apples|{1} There is one apple|[1,Inf] There is more than one apple', 0.0],
-//        ['There are no apples', '{0.0} There are no apples|]0,1[There are %count% apples|{1} There is one apple|[1,Inf] There is more than one apple', 0]
+        ['There is almost one apple', '{0} There are no apples|]0,1[ There is almost one apple|{1} There is one apple|[1,Inf] There is more than one apple', 0.7],
+        ['There is one apple', '{0} There are no apples|]0,1[There are %count% apples|{1} There is one apple|[1,Inf] There is more than one apple', 1],
+        ['There is more than one apple', '{0} There are no apples|]0,1[There are %count% apples|{1} There is one apple|[1,Inf] There is more than one apple', 1.7],
+        ['There are no apples', '{0} There are no apples|]0,1[There are %count% apples|{1} There is one apple|[1,Inf] There is more than one apple', 0],
+        ['There are no apples', '{0} There are no apples|]0,1[There are %count% apples|{1} There is one apple|[1,Inf] There is more than one apple', 0.0],
+        ['There are no apples', '{0.0} There are no apples|]0,1[There are %count% apples|{1} There is one apple|[1,Inf] There is more than one apple', 0]
     ];
 }
 
-test("trans default domain test", function () {
-    var tests = getDefaultDomainPhrases();
-
-    while (tests.length) {
-        var test = tests.shift();
-        var actual = test[1].trans(test[2]);
-        var expected = test[0];
-        strictEqual(actual, expected);
+module("String.trans()", {
+    setup: function() {
+    // prepare something for all following tests
+    },
+    teardown: function() {
+// clean up after each test
     }
 });
 
-test("transchoice choose test", function () {
+test("basic test", function () {
+    var tests = getTransBasic();
+
+    while (tests.length) {
+        var test = tests.shift();
+        var message = test[1];
+
+        var actual = message.trans();
+        var expected = test[0];
+        strictEqual(actual, expected, test[2]);
+    }
+});
+
+test("default domain test", function () {
+    var tests = getDefaultDomain();
+
+    while (tests.length) {
+        var test = tests.shift();
+        var message = test[1];
+        var attributes = test[2];
+
+        var actual = message.trans(attributes);
+        var expected = test[0];
+        strictEqual(actual, expected, test[3]);
+    }
+});
+
+test("given domain test", function () {
+    var tests = getGivenDomain();
+
+    while (tests.length) {
+        var test = tests.shift();
+        var message = test[1];
+        var attributes = test[2];
+        var domain = test[3];
+
+        var actual = message.trans(attributes, domain);
+        var expected = test[0];
+        strictEqual(actual, expected, test[4]);
+    }
+});
+
+module("String.transchoice()", {
+    setup: function () {
+// prepare something for all following tests
+    },
+    teardown: function () {
+// clean up after each test
+    }
+});
+
+test("not matching messages test", function () {
+    var tests = getNonMatchingMessages();
+
+    while (tests.length) {
+        var test = tests.shift();
+        var message = test[0];
+        var count = test[1];
+
+        throws(
+            function() {
+                message.transchoice(count, {}, 'TranschoiceChooseTest');
+            },
+            /^Unable to choose a translation for/,
+            'raised error message starts with "Unable to choose a translation for".'
+        );
+    }
+});
+
+test("choose test", function () {
     var tests = getChooseTests();
 
     while (tests.length) {
@@ -97,6 +192,6 @@ test("transchoice choose test", function () {
         var count = test[2];
         var actual = test[1].transchoice(count, {}, 'TranschoiceChooseTest');
         var expected = test[0].replace(new RegExp('%count%', 'mg'), count);
-        strictEqual(actual, expected, test[1] + ' | ' + count);
+        strictEqual(actual, expected, actual + ' | ' + count);
     }
 });
--- Resources/public/js/trans.js
+++ Resources/public/js/trans.js
@@ -14,38 +14,44 @@ if (typeof String.prototype.trans === 'undefined') {
         domain = domain || null;
         locale = locale || this.locale;
 
-        if (domain === null) {
+        var rx = null;
+
+        if (null === domain) {
             domain = 'messages';
         }
 
-        var phrase = null;
+        var message = null;
 
         // Search for phrase match:
         if (this.translations.hasOwnProperty(locale) && this.translations[locale].hasOwnProperty(domain) && this.translations[locale][domain].hasOwnProperty(this.toString())) {
-            phrase = this.translations[locale][domain][this.toString()];
+            message = this.translations[locale][domain][this.toString()];
         }
 
         // No translation found:
-        if (phrase === null) {
-            phrase = this.toString();
+        if (message === null) {
+            message = this.toString();
         }
 
         // Optional partial arguments:
         for (var key in arguments) {
             if (arguments.hasOwnProperty(key)) {
-                var rx = new RegExp(key, 'g');
-                phrase = phrase.replace(rx, arguments[key]);
+                rx = new RegExp(key, 'g');
+                message = message.replace(rx, arguments[key]);
             }
         }
 
-        return phrase.toString();
+        return message.toString();
+    };
+
+    String.prototype.transchoiceException = function () {
+        var message = 'Unable to choose a translation for "__PHRASE__" with locale "__LOCALE__". Double check that this translation has the correct plural options (e.g. "There is one apple|There are %count% apples").';
+        message = message.replace(/__PHRASE__/, this.toString());
+        message = message.replace(/__LOCALE__/, this.locale);
+
+        return message;
     };
 
     String.prototype.transchoice = function (count, arguments, domain, locale) {
-
-        var exception = 'Unable to choose a translation for "__PHRASE__" with locale "__LOCALE__". Double check that this translation has the correct plural options (e.g. "There is one apple|There are %count% apples").';
-        exception.replace(/__PHRASE__/, this.toString());
-        exception.replace(/__LOCALE__/, this.locale);
 
         count = count || 0;
         arguments = arguments || {};
@@ -53,137 +59,221 @@ if (typeof String.prototype.trans === 'undefined') {
         domain = domain || null;
         locale = locale || this.locale;
 
-        var rx = null;
-
         if (null === domain) {
             domain = 'messages';
         }
 
-        var mainphrase = this.toString();
+        var message = this.trans(arguments, domain, locale);
 
-        // Search for phrase match:
-        if (this.translations.hasOwnProperty(domain) && this.translations[domain].hasOwnProperty(mainphrase)) {
-            mainphrase = this.translations[domain][mainphrase];
+        return MessageSelector.choose(message, count, locale);
+    }
+}
+
+
+// /vendor/symfony/symfony/src/Symfony/Component/Translation/Interval.php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// namespace Symfony\Component\Translation;
+
+/**
+ * Tests if a given number belongs to a given math interval.
+ *
+ * An interval can represent a finite set of numbers:
+ *
+ *  {1,2,3,4}
+ *
+ * An interval can represent numbers between two numbers:
+ *
+ *  [1, +Inf]
+ *  ]-1,2[
+ *
+ * The left delimiter can be [ (inclusive) or ] (exclusive).
+ * The right delimiter can be [ (exclusive) or ] (inclusive).
+ * Beside numbers, you can use -Inf and +Inf for the infinite.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ *
+ * @see    http://en.wikipedia.org/wiki/Interval_%28mathematics%29#The_ISO_notation
+ */
+Interval = {
+    /**
+     * Tests if the given number is in the math interval.
+     *
+     * @param {integer} $number   A number
+     * @param {string}  $interval An interval
+     *
+     * @return {Boolean}
+     *
+     * @throws {string}
+     */
+    test: function ($number, $interval) {
+        $interval = $interval.trim();
+
+        var $rx = new XRegExp(Interval.getIntervalRegexp(), 'x');
+        var $matches = XRegExp.exec($interval, $rx);
+
+        if ($matches === null) {
+            throw '"%s" is not a valid interval.'.replace(/%s/, $interval);
         }
 
-        // Optional partial arguments:
-        for (var key in arguments) {
-            if (arguments.hasOwnProperty(key)) {
-                rx = new RegExp(key, 'g');
-                mainphrase = mainphrase.toString().replace(rx, arguments[key]);
+        if ($matches[1]) {
+            var temp = $matches[2].split(',');
+            while (temp.length) {
+                var $n = temp.shift();
+                if ($number == $n) {
+                    return true;
+                }
             }
+        } else {
+            var $leftNumber = this.convertNumber($matches['left']);
+            var $rightNumber = this.convertNumber($matches['right']);
+
+            return ('[' === $matches['left_delimiter'] ? $number >= $leftNumber : $number > $leftNumber)
+                && (']' === $matches['right_delimiter'] ? $number <= $rightNumber : $number < $rightNumber)
+                ;
         }
 
-        var temp = mainphrase.split('|');
+        return false;
+    },
 
-        if (temp.length === 1) {
-            return mainphrase;
+    /**
+     * Returns a Regexp that matches valid intervals.
+     *
+     * @return string A Regexp (without the delimiters)
+     */
+    getIntervalRegexp: function () {
+        var rx = ''
+            + '({\\s*(\\-?\\d+(\\.\\d+)?[\\s*,\\s*\\-?\\d+(\\.\\d+)?]*)\\s*})'
+            + '|'
+            + '(?<left_delimiter>[\\[\\]])\\s*(?<left>-Inf|\\-?\\d+(\\.\\d+)?)\\s*,\\s*(?<right>\\+?Inf|\\-?\\d+(\\.\\d+)?)\\s*(?<right_delimiter>[\\[\\]])'
+            + '';
+
+        return rx;
+    },
+
+    convertNumber: function ($number) {
+        if ('-Inf' === $number) {
+            return -Infinity;
+        } else if ('+Inf' === $number || 'Inf' === $number) {
+            return Infinity;
         }
 
-        var wordmap = {
-            'none:': '{0}',
-            'one:': '{1}',
-            'more:': ']1,Inf]'
-        };
+        return parseFloat($number);
+    }
+};
 
-        var parts = {};
-        var counter = 0;
+PluralizationRules = {
+    get: function ($number, $locale) {
+        return ($number == 1) ? 0 : 1;
+    }
+};
 
-        for (var i = 0; i < temp.length; i++) {
-            var chunk = temp[i].trim();
-            rx = new RegExp('^((?:one:)|(?:more:)|(?:{[^}]+})|(?:(?:])(?:.*?)(?:])))(.*?)$', 'mg');
-            var range = chunk.replace(rx, '$1');
-            chunk = chunk.replace(rx, '$2');
+// /vendor/symfony/symfony/src/Symfony/Component/Translation/MessageSelector.php
 
-            if (range === chunk) {
-                range = counter > 0 ? '{' + count + '}' : '{1}';
-                counter++;
-            }
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-            chunk = chunk.trim();
-            range = range.replace(/\s*/g, '');
-            range = wordmap.hasOwnProperty(range) ? wordmap[range] : range;
-            range = range.replace(/{(\d+)}/, '$1');
+// namespace Symfony\Component\Translation;
 
-            parts[range] = chunk;
-        }
+/**
+ * MessageSelector.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ *
+ * @api
+ */
+MessageSelector = {
+    /**
+     * Given a message with different plural translations separated by a
+     * pipe (|), this method returns the correct portion of the message based
+     * on the given number, locale and the pluralization rules in the message
+     * itself.
+     *
+     * The message supports two different types of pluralization rules:
+     *
+     * interval: {0} There are no apples|{1} There is one apple|]1,Inf] There are %count% apples
+     * indexed:  There is one apple|There are %count% apples
+     *
+     * The indexed solution can also contain labels (e.g. one: There is one apple).
+     * This is purely for making the translations more clear - it does not
+     * affect the functionality.
+     *
+     * The two methods can also be mixed:
+     *     {0} There are no apples|one: There is one apple|more: There are %count% apples
+     *
+     * @param {string}  $message The message being translated
+     * @param {integer} $number  The number of items represented for the message
+     * @param {string}  $locale  The locale to use for choosing
+     *
+     * @return {string}
+     *
+     * @throws {string}
+     *
+     * @api
+     */
+    choose: function ($message, $number, $locale) {
+        $parts = $message.split('|');
+        $explicitRules = {};
+        $standardRules = [];
 
-        var phrase = null;
+        for (var $i = 0; $i < $parts.length; $i++) {
+            var $part = $parts[$i];
+            $part = $part.trim();
 
-        // Simple numeric value:
-        if (parts.hasOwnProperty(count)) {
-            return parts[count].toString();
-        }
+            var $rx = new XRegExp('^(?<interval>' + Interval.getIntervalRegexp() + ')\\s*(?<message>.*?)$', 'x');
+            var $matches = XRegExp.exec($part, $rx);
 
-        function parseValue(value) {
-            switch (value) {
-                case '-Inf':
-                    return -Infinity;
-                case '+Inf':
-                    return Infinity;
-                case 'Inf':
-                    return Infinity;
-            }
 
-            return parseFloat(value);
-        }
+            if ($matches !== null) {
+                $explicitRules[$matches['interval']] = $matches['message'];
+            } else {
+                $rx = new XRegExp('^\\w+\\:\\s*(.*?)$', 'x');
+                $matches = XRegExp.exec($part, $rx);
 
-        // Range match:
-        for (var index in parts) {
-            if (parts.hasOwnProperty(index) && isNaN(index)) {
-                var r = index.split(',');
-
-                var left = {
-                    operator: parseLeftOperator(r[0].replace(/^(.)(.*?)$/, '$1')),
-                    value: parseValue(r[0].replace(/^(.)(.*?)$/, '$2'))
-                };
-
-                var right = {
-                    operator: parseRightOperator(r[1].replace(/^(.*?)(.)$/, '$2')),
-                    value: parseValue(r[1].replace(/^(.*?)(.)$/, '$1'))
-                };
-
-                if (left.operator(count) && right.operator(count)) {
-                    phrase = parts[index];
-                    break;
+                if ($matches !== null) {
+                    $standardRules.push($matches[1]);
+                } else {
+                    $standardRules.push($part);
                 }
             }
         }
 
-        function parseLeftOperator(operator) {
-            switch (operator) {
-                case ']':
-                    return function (val) {
-                        return this.value !== -Infinity && this.value !== Infinity ? val > this.value : false;
-                    };
-                case '[':
-                    return function (val) {
-                        return val >= this.value;
-                    };
-            }
+        // try to match an explicit rule, then fallback to the standard ones
+        for (var $interval in $explicitRules) {
+            var $m = $explicitRules[$interval];
 
-            return function () {
-                return false;
+            if (Interval.test($number, $interval)) {
+                return $m;
             }
         }
 
-        function parseRightOperator(operator) {
-            switch (operator) {
-                case ']':
-                    return function (val) {
-                        return val <= this.value;
-                    };
-                case '[':
-                    return function (val) {
-                        return this.value !== -Infinity && this.value !== Infinity ? val > this.value : false;
-                    };
+        $position = PluralizationRules.get($number, $locale);
+
+        if (typeof $standardRules[$position] === 'undefined') {
+            // when there's exactly one rule given, and that rule is a standard
+            // rule, use this rule
+            if (1 === $parts.length && typeof $standardRules[0] !== 'undefined') {
+                return $standardRules[0];
             }
 
-            return function () {
-                return false;
-            }
+            throw $message.transchoiceException();
         }
 
-        return phrase;
+        return $standardRules[$position];
     }
-}
+};
