313e9f1150c7a30263e8cef8e62559a4c776bc4d
Jacek Siciarek <siciarek@gmail.com>
2017-10-01 00:17:14 +0200

    Update SiciarekJsTransExtension.php

--- Twig/Extension/SiciarekJsTransExtension.php
+++ Twig/Extension/SiciarekJsTransExtension.php
@@ -2,17 +2,34 @@
 
 namespace Siciarek\JsTransBundle\Twig\Extension;
 
-use Symfony\Component\DependencyInjection\Container;
+use Symfony\Bundle\FrameworkBundle\Templating\Helper\AssetsHelper;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Translation\Translator;
 
 class SiciarekJsTransExtension extends \Twig_Extension
 {
+    /**
+     * @var Translator
+     */
+    protected $translator;
+    /**
+     * @var \Twig_Environment
+     */
+    protected $twig;
+    /**
+     * @var string
+     */
+    protected $translationsDir;
+    /**
+     * @var callable
+     */
+    protected $assetFunction;
 
-    protected $container;
-
-    public function __construct(Container $container)
+    public function __construct(Translator $translator, \Twig_Environment $twig, $translationsDir)
     {
-        $this->container = $container;
+        $this->translator = $translator;
+        $this->twig = $twig;
+        $this->translationsDir = $translationsDir;
     }
 
     /**
@@ -44,28 +61,30 @@ class SiciarekJsTransExtension extends \Twig_Extension
     }
 
     /**
-     * Custom methods
+     * Translations helper
+     *
+     * @param $currlocale
+     * @param array $locs
+     * @return string
      */
-    public function translations($currlocale, $locs = array())
+    public function translations($currlocale, $locs = [])
     {
-        $directory = $this->container->get('kernel')->getCacheDir() . DIRECTORY_SEPARATOR . 'translations';
-
         if (!in_array($currlocale, $locs)) {
             $locs[] = $currlocale;
         }
 
-        $catalogues = array();
-
+        # Just building translation catalogues for all the languages in $loc
         foreach ($locs as $loc) {
-            $this->container->get('translator')->trans(null, array(), null, $loc);
+            $this->translator->trans(null, [], null, $loc);
         }
 
         $finder = new Finder();
-        $files = $finder->files()->in($directory)->name('/\.php$/');
+        $files = $finder->files()->in($this->translationsDir)->name('/\.php$/');
+
+        $catalogues = [];
 
         foreach ($files as $file) {
             $script = $file->getRealPath();
-
             $loc = preg_replace('/.*catalogue\.([^\.]+)\..*/', '$1', $script);
 
             /**
@@ -75,14 +94,31 @@ class SiciarekJsTransExtension extends \Twig_Extension
             $catalogues[$loc] = $catalogue->all();
         }
 
-        $json = json_encode($catalogues);
+        $translations = json_encode($catalogues);
+        $xregexpJs = $this->asset('bundles/siciarekjstrans/js/lib/xregexp.min.js');
+        $transJs = $this->asset('bundles/siciarekjstrans/js/dist/trans.min.js');
 
-        $output = [];
+        return <<<ASSETS
+<script>String.prototype.locale = '{$currlocale}';</script>
+    <script>String.prototype.translations = {$translations};</script>
+    <script src="{$xregexpJs}"></script>
+    <script src="{$transJs}"></script>
+ASSETS;
 
-        $output[] = sprintf('<script>String.prototype.locale = "%s";</script>', $currlocale);
-        $output[] = sprintf('<script>String.prototype.translations = %s;</script>', $json);
-
-        return implode("\n", $output);
     }
 
+    /**
+     * Asset function wrapper
+     *
+     * @param $asset
+     * @return mixed
+     */
+    protected function asset($asset)
+    {
+        if (empty($this->assetFunction)) {
+            $this->assetFunction = $this->twig->getFunction('asset')->getCallable();
+        }
+
+        return call_user_func($this->assetFunction, $asset);
+    }
 }
